a14d95c8051658c94e1142148683f7b582085f80
Rafa Canosa <rafacv23@gmail.com>
2024-04-13 18:52:45 +0200

    styles

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function footer() {
   return (
-    <footer className="flex items-start justify-center py-4 bg-gray-800">
+    <footer className="flex items-start justify-center py-4 border-t border-t-gray-800 rounded-xl">
       Esto es el footer
     </footer>
   )
