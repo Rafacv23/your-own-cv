fd6324b3c69cfe7758a5de4e1240f2bacbcb91d6
Rafa Canosa <rafacv23@gmail.com>
2024-04-13 18:00:39 +0200

    footer test

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function footer() {
+  return (
+    <footer className="flex items-start justify-center py-4 bg-gray-800">
+      Esto es el footer
+    </footer>
+  )
+}
