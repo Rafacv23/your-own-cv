b3da7294f8b0b6d4d52f93e6bdf6c952b1a60c43
Rafa Canosa <rafacv23@gmail.com>
2024-04-15 17:19:32 +0200

    interface typescript

--- app/components/create/ContactData.tsx
+++ app/components/create/ContactData.tsx
@@ -2,10 +2,19 @@ import React from "react"
 import { BsFillPhoneVibrateFill } from "react-icons/bs"
 import { MdMarkEmailRead } from "react-icons/md"
 import { CgWebsite } from "react-icons/cg"
-import { BiWorld } from "react-icons/bi"
-import { BiSolidContact } from "react-icons/bi"
+import { BiWorld, BiSolidContact } from "react-icons/bi"
 
-export default function ContactData({ data }) {
+// Define interface for data prop
+interface ContactDataProps {
+  data: {
+    email: string
+    telefono: string
+    web: string
+    pais: string
+  }
+}
+
+export const ContactData: React.FC<ContactDataProps> = ({ data }) => {
   return (
     <aside className="flex flex-col gap-4">
       <h2 className="text-2xl text-violet-400 font-semibold mb-4 flex gap-4 items-center">
