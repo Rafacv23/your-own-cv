c52d8bf657b876eca1148d6621de3a45e2b8b5bf
Rafa Canosa <rafacv23@gmail.com>
2024-04-15 17:08:18 +0200

    reformated

--- app/components/create/CvPreview.tsx
+++ app/components/create/CvPreview.tsx
@@ -1,42 +1,58 @@
 import React from "react"
-
+import { useRef } from "react"
+import html2pdf from "html2pdf.js"
+import TrabajosList from "./TrabajosList"
+import EstudiosList from "./EstudiosList"
+import HabilidadesList from "./HabilidadesList"
+import ContactData from "./ContactData"
 export default function CvPreview({ data }) {
+  const contentRef = useRef(null)
+
+  const options = {
+    filename: "your-own-cv.pdf",
+    margin: 0.5,
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "A4", orientation: "portrait" },
+  }
+
+  const convertToPdf = () => {
+    const content = contentRef.current
+
+    html2pdf().set(options).from(content).save()
+  }
+
   return (
-    <article className="flex flex-col gap-4">
-      <h1 className="text-3xl">
-        {data.nombre} {data.apellidos}
-      </h1>
-      <div className="flex gap-4">
-        <aside>
-          <h2 className="text-2xl">Contacto</h2>
-          <h3>{data.email}</h3>
-          <h3>{data.telefono}</h3>
-          <h3>{data.web}</h3>
-          <h3>{data.pais}</h3>
-        </aside>
-        <main>
-          {data.descripcion}
-          <h2 className="text-2xl">Experiencia</h2>
-          {data.trabajos
-            ? data.trabajos.map((trabajo) => (
-                <li key={trabajo.trabajo}>{trabajo.trabajo}</li>
-              ))
-            : null}
-          <h2 className="text-2xl">Estudios</h2>
-          {data.estudios
-            ? data.estudios.map((estudio) => (
-                <li key={estudio.titulacion}>{estudio.titulacion}</li>
-              ))
-            : null}
-          <h2 className="text-2xl">Habilidades</h2>
-          {data.habilidades
-            ? data.habilidades.map((habilidad) => (
-                <li key={habilidad.habilidad}>{habilidad.habilidad}</li>
-              ))
-            : null}
-        </main>
-      </div>
-      <button className="bg-violet-400 p-2 rounded">Descargar</button>
-    </article>
+    <div className="flex flex-col gap-4">
+      <article
+        className="flex flex-col gap-4 text-white bg-black p-8 h-full"
+        ref={contentRef}
+      >
+        <h1 className="text-3xl font-bold boder-b border-b-gray-800 boder-b">
+          {data.nombre} {data.apellidos}
+        </h1>
+        <p>{data.descripcion}</p>
+        <div className="flex gap-12">
+          <ContactData data={data} />
+          <main>
+            {data.trabajos.length >= 1 ? (
+              <TrabajosList trabajos={data.trabajos} />
+            ) : null}
+            {data.estudios.length >= 1 ? (
+              <EstudiosList estudios={data.estudios} />
+            ) : null}
+            {data.habilidades.length >= 1 ? (
+              <HabilidadesList habilidades={data.habilidades} />
+            ) : null}
+          </main>
+        </div>
+      </article>
+      <button
+        className=" bg-violet-400 rounded p-4 font-bold text-black"
+        onClick={convertToPdf}
+      >
+        Descargar
+      </button>
+    </div>
   )
 }
