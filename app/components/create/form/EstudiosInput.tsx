dad3a6e2bb3ce579105e237b38e22d31eb4cb28f
Rafa Canosa <rafacv23@gmail.com>
2024-04-16 16:55:26 +0200

    new components for the sections of the create form

---
+++ app/components/create/form/EstudiosInput.tsx
@@ -0,0 +1,75 @@
+import { Estudio } from "@/app/utils/definitions"
+import React from "react"
+import { SetStateAction, Dispatch } from "react"
+import { Input } from "@nextui-org/input"
+import { Button } from "@nextui-org/button"
+import { Textarea } from "@nextui-org/react"
+import {
+  handleAddEstudio,
+  handleEstudioChange,
+} from "@/app/utils/handleFunctions"
+
+export default function EstudiosInput({
+  estudios,
+  setEstudios,
+}: {
+  estudios: Estudio[]
+  setEstudios: Dispatch<SetStateAction<Estudio[]>>
+}) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-violet-400">Estudios</h2>
+      {estudios.map((estudio, index) => (
+        <div key={index} className="flex flex-col gap-4">
+          <Input
+            type="text"
+            label="Titulación"
+            name="titulo"
+            isRequired
+            value={estudio.titulo}
+            onChange={(e) =>
+              handleEstudioChange(index, e, estudios, setEstudios)
+            }
+          />
+          <Input
+            type="text"
+            label="Centro de estudios"
+            name="escuela"
+            isRequired
+            value={estudio.escuela}
+            onChange={(e) =>
+              handleEstudioChange(index, e, estudios, setEstudios)
+            }
+          />
+          <Input
+            type="date"
+            label="Fecha de entrada"
+            name="fecha"
+            className="text-black"
+            value={estudio.fecha}
+            onChange={(e) =>
+              handleEstudioChange(index, e, estudios, setEstudios)
+            }
+          />
+          <Textarea
+            name="descripcion"
+            placeholder="¿Qué aprendiste?"
+            label="Descripción"
+            value={estudio.descripcion}
+            onChange={(e) =>
+              handleEstudioChange(index, e, estudios, setEstudios)
+            }
+          />
+        </div>
+      ))}
+      <Button
+        color="secondary"
+        variant="ghost"
+        type="button"
+        onClick={() => handleAddEstudio(setEstudios, estudios)}
+      >
+        Añadir estudios
+      </Button>
+    </section>
+  )
+}
---
+++ app/components/create/form/HabilidadesInput.tsx
@@ -0,0 +1,45 @@
+import { Habilidad } from "@/app/utils/definitions"
+import React from "react"
+import { SetStateAction, Dispatch } from "react"
+import { Input } from "@nextui-org/input"
+import { Button } from "@nextui-org/button"
+import {
+  handleAddHabilidad,
+  handleHabilidadChange,
+} from "@/app/utils/handleFunctions"
+
+export default function HabilidadesInput({
+  habilidades,
+  setHabilidades,
+}: {
+  habilidades: Habilidad[]
+  setHabilidades: Dispatch<SetStateAction<Habilidad[]>>
+}) {
+  return (
+    <section>
+      <h2 className="text-2xl font-bold text-violet-400">Habilidades</h2>
+      {habilidades.map((habilidad, index) => (
+        <div key={index} className="flex flex-col gap-4">
+          <Input
+            type="text"
+            label="Habilidad"
+            name="habilidad"
+            isRequired
+            value={habilidad.habilidad}
+            onChange={(e) =>
+              handleHabilidadChange(index, e, habilidades, setHabilidades)
+            }
+          />
+        </div>
+      ))}
+      <Button
+        color="secondary"
+        variant="ghost"
+        type="button"
+        onClick={() => handleAddHabilidad(setHabilidades, habilidades)}
+      >
+        Añadir habilidad
+      </Button>
+    </section>
+  )
+}
