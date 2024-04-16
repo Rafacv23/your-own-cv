0347864a756234ca1c7b0e521b19b3d5d5f464d6
Rafa Canosa <rafacv23@gmail.com>
2024-04-16 16:58:30 +0200

    new name and changed content

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function page() {
+export default function Example() {
   return (
     <main className="min-h-screen w-full gap-4 flex flex-col sm:flex-row items-center justify-center ">
       <object
@@ -10,11 +10,10 @@ export default function page() {
         height="500px"
       >
         <p>
-          Unable to display PDF file.{" "}
+          No se ha podido mostrar el archivo.{" "}
           <a href="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf">
-            Download
-          </a>{" "}
-          instead.
+            Descargar.
+          </a>
         </p>
       </object>
     </main>
