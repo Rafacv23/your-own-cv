d4eecab72010274a6c28bc41ca9e77318794b38e
Rafa Canosa <rafacv23@gmail.com>
2024-04-16 15:39:30 +0200

    icons and back to top btn

--- app/components/home/Content.tsx
+++ app/components/home/Content.tsx
@@ -1,11 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react"
 import Link from "next/link"
+import {
+  AiFillSetting,
+  AiFillCode,
+  AiFillGithub,
+  AiOutlineArrowUp,
+} from "react-icons/ai"
+import { Button } from "@nextui-org/react"
+import { MdMoneyOff, MdRocketLaunch } from "react-icons/md"
 
 export default function Content() {
   return (
     <>
-      <section className=" max-w-3xl mx-8 md:flex-row flex flex-col gap-4 h-screen items-center justify-center">
+      <section
+        id="start"
+        className=" max-w-3xl mx-8 md:flex-row flex flex-col gap-4 h-screen items-center justify-center"
+      >
         <div className="flex flex-col items-start justify-center gap-4">
           <h1 className="text-3xl">
             <span className="text-violet-400 font-bold">Your Own CV</span> tu
@@ -15,13 +26,27 @@ export default function Content() {
             Your Own CV te permite crear tu propio currículum completamente
             personalizado y gratis.
           </p>
-          <Link
-            href="/create"
-            title="Crear"
-            className=" bg-violet-400 rounded p-4 font-bold text-black"
-          >
-            Crear
-          </Link>
+          <div className="flex gap-4 items-center">
+            <Link href="/create" title="Crear">
+              <Button color="secondary" startContent={<MdRocketLaunch />}>
+                Crear
+              </Button>
+            </Link>
+            <Link
+              href={"https://github.com/Rafacv23/your-own-cv"}
+              title="Repositorio de Github"
+              target="blank"
+            >
+              <Button
+                color="default"
+                variant="bordered"
+                className="text-white"
+                startContent={<AiFillGithub />}
+              >
+                Github
+              </Button>
+            </Link>
+          </div>
         </div>
         <img
           className="rounded h-auto"
@@ -31,40 +56,61 @@ export default function Content() {
           width={200}
         />
       </section>
-      <section className=" md:h-screen max-w-3xl mb-24">
-        <h2 className="text-2xl text-violet-400 font-bold mb-8">
+      <section className="md:h-screen max-w-3xl mb-24">
+        <h2 className="text-2xl text-violet-400 font-bold mb-8 mx-16 md:mx-0">
           ¿Por qué usar Your Own CV?
         </h2>
-        <ul className="flex flex-col md:grid xl:grid-cols-3 md:grid-cols-2 gap-4 items-start justify-center">
-          <li className="text-lg bg-gray-900 p-4 rounded shadow-lg">
-            <h3 className="text-xl mb-4 font-semibold">⚙️ Personalizable</h3>
-            <p className="mb-4 text-violet-200">fdasdfadadadadadasdadad</p>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-16 md:mx-0">
+          <li className="flex flex-col bg-gray-900 p-4 rounded shadow-lg">
+            <h3 className="text-xl mb-4 font-semibold flex gap-2 items-center">
+              <AiFillSetting /> Personalizable
+            </h3>
+            <p className="mb-4 text-violet-200">
+              Personaliza tu currículum para destacar tus habilidades y
+              experiencia de manera única y profesional.
+            </p>
             <img
               src="/home-2.avif"
               alt=""
-              className="rounded max-h-56 max-w-48"
+              loading="lazy"
+              className="rounded h-48 w-full object-cover"
             />
           </li>
-          <li className="text-lg bg-gray-900 p-4 rounded max-h-max">
-            <h3 className="text-lg mb-4 font-semibold">📖 Código abierto</h3>
-            <p className="mb-4 text-violet-200">fdasdfadadadadadasdadad</p>
+          <li className="flex flex-col bg-gray-900 p-4 rounded shadow-lg">
+            <h3 className="text-xl mb-4 font-semibold flex items-center gap-2">
+              <AiFillCode /> Código abierto
+            </h3>
+            <p className="mb-4 text-violet-200">
+              Accede al código fuente de Your Own CV y colabora en su desarrollo
+              para mejorar la plataforma y adaptarla a tus necesidades.
+            </p>
             <img
               src="/home-5.avif"
               alt=""
-              className="rounded max-h-56 max-w-48"
+              loading="lazy"
+              className="rounded h-48 w-full object-cover"
             />
           </li>
-          <li className="text-lg bg-gray-900 p-4 rounded">
-            <h3 className="text-lg mb-4 font-semibold">🆓 Gratis</h3>
-            <p className="mb-4 text-violet-200">fdasdfadadadadadasdadad</p>
+          <li className="flex flex-col bg-gray-900 p-4 rounded shadow-lg">
+            <h3 className="text-xl mb-4 font-semibold flex gap-2 items-center">
+              <MdMoneyOff /> Gratis
+            </h3>
+            <p className="mb-4 text-violet-200">
+              Crea tu currículum en línea de forma gratuita y en cuestión de
+              minutos, sin costos ocultos ni suscripciones.
+            </p>
             <img
               src="/home-4.avif"
               alt=""
-              className="rounded max-h-56 max-w-48"
+              loading="lazy"
+              className="rounded h-48 w-full object-cover"
             />
           </li>
         </ul>
       </section>
+      <Link href="#start" title="Volver al inicio" className="mb-8">
+        <Button startContent={<AiOutlineArrowUp />}>Volver</Button>
+      </Link>
     </>
   )
 }
