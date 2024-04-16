883b84fb25b0c4682cd457ef06f33fcf35337102
Rafa Canosa <rafacv23@gmail.com>
2024-04-16 15:05:42 +0200

    removed interface add import type

--- app/components/create/EstudiosList.tsx
+++ app/components/create/EstudiosList.tsx
@@ -1,21 +1,15 @@
 import React from "react"
 import { PiStudentFill } from "react-icons/pi"
+import { Estudio, Estudios } from "@/app/utils/definitions"
 
-interface estudio {
-  titulo: string
-  escuela: string
-  fecha: string
-  descripcion: string
-}
-
-export default function EstudiosList({ estudios }: any) {
+export default function EstudiosList({ estudios }: Estudios) {
   return (
     <section>
       <h2 className="text-2xl text-violet-400 font-semibold mb-4 flex gap-4 items-center">
         <PiStudentFill /> Estudios
       </h2>
       <ul className="list-none flex flex-col gap-4 mb-8 mt-4">
-        {estudios.map((estudio: estudio) => (
+        {estudios.map((estudio: Estudio) => (
           <li key={estudio.titulo}>
             <h3 className=" text-xl">
               {estudio.titulo} | {estudio.escuela}
--- app/components/create/HabilidadesList.tsx
+++ app/components/create/HabilidadesList.tsx
@@ -1,18 +1,14 @@
 import React from "react"
 import { GiSkills } from "react-icons/gi"
-
-interface habilidad {
-  habilidad: string
-}
-
-export default function HabilidadesList({ habilidades }: any) {
+import { Habilidades, Habilidad } from "@/app/utils/definitions"
+export default function HabilidadesList({ habilidades }: Habilidades) {
   return (
     <section>
       <h2 className="text-2xl text-violet-400 font-semibold mb-4 flex gap-4 items-center">
         <GiSkills /> Habilidades
       </h2>
       <ul className="list-none flex flex-col gap-4 mb-8 mt-4">
-        {habilidades.map((habilidad: habilidad) => (
+        {habilidades.map((habilidad: Habilidad) => (
           <li key={habilidad.habilidad}>{habilidad.habilidad}</li>
         ))}
       </ul>
