477d9eb9e0c5fe5f7fb294368406ddc487b82579
Rafa Canosa <rafacv23@gmail.com>
2024-04-16 15:39:18 +0200

    added icons

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import Link from "next/link"
 import { Divider } from "@nextui-org/divider"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
+import { MdOutlineWork } from "react-icons/md"
 
 export default function footer() {
   return (
@@ -10,17 +12,29 @@ export default function footer() {
           <h4 className="text-lg font-semibold">Contenido</h4>
           <ul>
             <li>
-              <Link href="/" title="Inicio">
+              <Link
+                href="/"
+                title="Inicio"
+                className="hover:text-violet-400 hover:transition-colors"
+              >
                 Inicio
               </Link>
             </li>
             <li>
-              <Link href="/create" title="Crear">
+              <Link
+                href="/create"
+                title="Crear"
+                className="hover:text-violet-400 hover:transition-colors"
+              >
                 Crear
               </Link>
             </li>
             <li>
-              <Link href="/example" title="Ejemplo">
+              <Link
+                href="/example"
+                title="Ejemplo"
+                className="hover:text-violet-400 hover:transition-colors"
+              >
                 Ejemplo
               </Link>
             </li>
@@ -35,7 +49,9 @@ export default function footer() {
                 href="https://github.com/Rafacv23/your-own-cv"
                 title="Repositorio de Github"
                 target="blank"
+                className="flex gap-2 items-center hover:text-violet-400 hover:transition-colors"
               >
+                <AiFillGithub />
                 Github
               </Link>
             </li>
@@ -44,7 +60,9 @@ export default function footer() {
                 href="https://www.linkedin.com/in/rafa-canosa-vallejo-6328a5194/"
                 title="Rafa Canosa Linkedin"
                 target="blank"
+                className="flex gap-2 items-center hover:text-violet-400 hover:transition-colors"
               >
+                <AiFillLinkedin />
                 Linkedin
               </Link>
             </li>
@@ -53,7 +71,9 @@ export default function footer() {
                 href="https://rafa-canosa-portfolio.vercel.app/"
                 title="Rafa Canosa Portfolio"
                 target="blank"
+                className="flex gap-2 items-center hover:text-violet-400 hover:transition-colors"
               >
+                <MdOutlineWork />
                 Portfolio
               </Link>
             </li>
