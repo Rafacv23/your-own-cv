3a11eb543354df915ae59f9add22867699943737
Rafa Canosa <rafacv23@gmail.com>
2024-04-17 13:07:22 +0200

    port to astro

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Content from "./components/home/Content"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen w-full gap-4 flex flex-col items-center justify-center">
-      <Content />
-    </main>
-  )
-}
