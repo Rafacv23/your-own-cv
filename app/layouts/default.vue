31ad9a9cfd2f4789a75b3e6aca1d4b1d9afdc947
Rafa Canosa <rafacv23@gmail.com>
2025-07-01 01:52:28 +0200

    style: enhance main section padding and minimum height for better layout consistency

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen max-w-6xl mx-4 md:mx-8 lg:mx-auto">
     <Header />
-    <main class="py-20 md:py-40">
+    <main class="py-20 md:py-40 lg:pt-60 xl:pt-80 lg:min-h-screen">
       <slot />
     </main>
     <Footer />
